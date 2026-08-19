f3cb90b7cca917fcad13b157de387752149f99ac
Juss <286642846+jussray@users.noreply.github.com>
2026-08-19 18:25:48 -0400

    fix(lint): declare worker crypto globals

--- worker/founder-content-learning-route.js
+++ worker/founder-content-learning-route.js
@@ -1,3 +1,4 @@
+/* global TextEncoder, crypto */
 import { buildFounderContentLearningSignal } from '../src/domain/founder-content-learning.js';
 import { BUILD_RELEASE_SHA } from './release-sha.js';
 
