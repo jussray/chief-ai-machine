523bdf72c99b7c77ee543857e0d4640083db796a
Juss <286642846+jussray@users.noreply.github.com>
2026-09-21 20:12:12 -0400

    feat(lawyer): export legal authority kernel

---
+++ plugins/lawyer/index.js
@@ -0,0 +1,14 @@
+export {
+  LEGAL_TRUTH_STATES,
+  assertVerifiedLawPromotion,
+  classifyLegalSource,
+  evaluateLegalFinding,
+  rankSourcesForResearch,
+  sourceCanVerifyLaw,
+} from './src/authority.js';
+
+export {
+  LEGAL_AREA_TAXONOMY,
+  LEGAL_BOOK_CATEGORIES,
+  LEGAL_SOURCE_FAMILIES,
+} from './src/source-registry.js';
